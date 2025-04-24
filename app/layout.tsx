14be248c7826c93241404bf9ac98548b002b9033
choni B <chonib@chonis-MacBook-Pro.local>
2025-04-25 00:10:00 +0300

    Fix Mantine CSS imports and remove Tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import '@mantine/core/styles.css'
-import '@mantine/notifications/styles.css'
-import '@mantine/dates/styles.css'
-import '@mantine/modals/styles.css'
+import '@mantine/core/styles.layer.css'
+import '@mantine/notifications/styles.layer.css'
+import '@mantine/dates/styles.layer.css'
+import '@mantine/modals/styles.layer.css'
 import './globals.css'
 
 import { ColorSchemeScript } from '@mantine/core'
